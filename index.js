40b04335943023fbe4f37ae62468e6433d3f7f2d
Stefania Formato <sformato95@gmail.com>
2025-11-18 22:17:54 +0100

    Complete project of sasso, carta e forbice

--- index.js
+++ index.js
@@ -1,2 +1,70 @@
 const sceltaCPU = document.querySelector(".scelta-cpu");
-const scelteUtente= document.querySelector(".scelta");
+const scelteUtente = document.querySelectorAll(".scelta");
+const messggio = document.querySelector(".messaggio");
+const btnGiocaDiNuovo = document.querySelector(".gioca-di-nuovo");
+
+scelteUtente.forEach(scelta => {
+
+    scelta.addEventListener("click", giocataCPU);
+
+});
+
+function giocataCPU(evento) {
+
+    const sceltaUtente = evento.target.dataset.scelta; // carta, forbice o sasso
+    const sceltePossibili = ["carta", "forbice", "sasso"];
+    const mossaCPU = sceltePossibili[generaNumeroRandomico(0, sceltePossibili.length - 1)];
+
+    switch (mossaCPU) {
+        case "carta":
+            sceltaCPU.innerText = "✋🏻";
+            break;
+        case "forbice":
+            sceltaCPU.innerText = "✌🏻";
+            break;
+        case "sasso":
+            sceltaCPU.innerText = "👊🏻";
+            break;
+    }
+
+    determinaVittoria(sceltaUtente, mossaCPU);
+
+}
+
+function generaNumeroRandomico(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+function determinaVittoria(mossaUtente, mossaCPU) {
+
+    if ((mossaUtente === "carta" && mossaCPU === "sasso") || (mossaUtente === "forbice " && mossaCPU === "carta") || (mossaUtente === "sasso" && mossaCPU === "forbice")) {
+        messggio.innerText = "Hai vinto! 🏆";
+    } else if ((mossaUtente === "sasso" && mossaCPU === "carta") || (mossaUtente === "carta" && mossaCPU === "forbice") || (mossaUtente === "forbice") && mossaCPU === "sasso") {
+
+        messggio.innerText = "La CPU ha vinto";
+    } else {
+        messggio.innerText = "Pareggio";
+    }
+    
+    mostraPulsanteGiocaDiNuovo();
+}
+
+// Aggiungo Event listner al bottone
+
+btnGiocaDiNuovo.addEventListener("click", function () {
+    btnGiocaDiNuovo.style.display = 'none';
+
+    const messaggio = document.querySelector(".messaggio");
+    messaggio.textContent = '';
+
+    const cpuDiv = document.querySelector(".scelta-cpu");
+    if (cpuDiv) {
+        cpuDiv.textContent = '';
+    }
+});
+
+function mostraPulsanteGiocaDiNuovo() {
+    btnGiocaDiNuovo.style.display = 'block';
+}
